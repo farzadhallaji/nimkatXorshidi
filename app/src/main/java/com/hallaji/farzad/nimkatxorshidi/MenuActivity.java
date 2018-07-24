de3c238fd6973fb90fc2f9728ed4d30af1cf2f4b
farzad <farzadhallaji@yahoo.com>
2018-07-24 22:52:52 +0430

    Menu complete

--- app/src/main/java/com/hallaji/farzad/nimkatxorshidi/MenuActivity.java
+++ app/src/main/java/com/hallaji/farzad/nimkatxorshidi/MenuActivity.java
@@ -99,96 +99,95 @@ public class MenuActivity extends AppCompatActivity {
 
                 finish();
                 switch (index){
-//                    case  0 :
-//                        Bundle mBundle = new Bundle();
-//                        mBundle.putString("url", "http://www.havairan.com/");
-//                        Intent intent0 = new Intent(getApplication() , Main4Activity.class);
-//                        intent0.putExtras(mBundle);
-//                        startActivity(intent0);
-//                        countDownTimer.cancel();
-//                        break;
+                    case  0 :
+                        Bundle mBundle = new Bundle();
+                        mBundle.putString("url", "http://www.havairan.com/");
+                        Intent intent0 = new Intent(getApplication() , WebPageActivity.class);
+                        intent0.putExtras(mBundle);
+                        startActivity(intent0);
+                        countDownTimer.cancel();
+                        break;
 //                    case  0 :
 //                        Intent intent0 = new Intent(getApplication() , com.hallaji.farzad.ehsan.w3bshark.activities.MainActivity.class);
 //                        startActivity(intent0);
 //                        countDownTimer.cancel();
 //                        break;
 //
-//                    case  1 :
-//                        Bundle mBundle1 = new Bundle();
-//                        mBundle1.putString("url", "http://techpark.ir/?login=true");
-//                        Intent intent1 = new Intent(getApplication() , Main4Activity.class);
-//                        intent1.putExtras(mBundle1);
-//                        startActivity(intent1);
-//                        countDownTimer.cancel();
-//                        break;
-//                    case  2 :
-//                        Bundle mBundle2 = new Bundle();
-//                        mBundle2.putString("url", "https://www.google.com/maps/place/Pardis+Technology+Park/@35.7298978,51.8227088,17z/data=!3m1!4b1!4m5!3m4!1s0x3f91da3b00f3efc9:0x383d79f4cad589c8!8m2!3d35.7298935!4d51.8248975");
-//                        Intent intent2 = new Intent(getApplication() , Main4Activity.class);
-//                        intent2.putExtras(mBundle2);
-//                        startActivity(intent2);
-//                        countDownTimer.cancel();
-//                        break;
-//                    case  3 :
-//                        Bundle mBundle3 = new Bundle();
-//                        mBundle3.putString("url", "http://www.online-traffic.ir/");
-//                        Intent intent3 = new Intent(getApplication() , Main4Activity.class);
-//                        intent3.putExtras(mBundle3);
-//                        startActivity(intent3);
-//                        countDownTimer.cancel();
-//                        break;
-//
-//                    case 4 :
-//                        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.sabaidea.aparat");
-//                        if (launchIntent != null) {
-//                            finish();
-//                            startActivity(launchIntent);//null pointer check in case package name was not found
-//                        }
-//                        startActivity(launchIntent);
-//
-//                        countDownTimer.cancel();
-//                        break;
-//
-//                    case 5 :
-//                        Bundle mBundle5 = new Bundle();
-//                        mBundle5.putString("ax", "metro");
-//                        Intent intent5 = new Intent(getApplication() , Main5Activity.class);
-//                        intent5.putExtras(mBundle5);
-//                        startActivity(intent5);
-//                        countDownTimer.cancel();
-//                        break;
-//
-//                    case 6 :
-////                        Bundle mBundle6 = new Bundle();
-////                        mBundle6.putString("ax", "brt");
-////                        Intent intent6 = new Intent(getApplication() , Main5Activity.class);
-////                        intent6.putExtras(mBundle6);
-////                        startActivity(intent6);
-////                        countDownTimer.cancel();
-////                        break;
-//
-//                        Intent intent6 = new Intent(getApplication() , Main6Activity.class);
+                    case  1 :
+                        Bundle mBundle1 = new Bundle();
+                        mBundle1.putString("url", "http://techpark.ir/?login=true");
+                        Intent intent1 = new Intent(getApplication() , WebPageActivity.class);
+                        intent1.putExtras(mBundle1);
+                        startActivity(intent1);
+                        countDownTimer.cancel();
+                        break;
+                    case  2 :
+                        Bundle mBundle2 = new Bundle();
+                        mBundle2.putString("url", "https://www.google.com/maps/place/Pardis+Technology+Park/@35.7298978,51.8227088,17z/data=!3m1!4b1!4m5!3m4!1s0x3f91da3b00f3efc9:0x383d79f4cad589c8!8m2!3d35.7298935!4d51.8248975");
+                        Intent intent2 = new Intent(getApplication() , WebPageActivity.class);
+                        intent2.putExtras(mBundle2);
+                        startActivity(intent2);
+                        countDownTimer.cancel();
+                        break;
+                    case  3 :
+                        Bundle mBundle3 = new Bundle();
+                        mBundle3.putString("url", "http://www.online-traffic.ir/");
+                        Intent intent3 = new Intent(getApplication() , WebPageActivity.class);
+                        intent3.putExtras(mBundle3);
+                        startActivity(intent3);
+                        countDownTimer.cancel();
+                        break;
+
+                    case 4 :
+                        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.sabaidea.aparat");
+                        if (launchIntent != null) {
+                            finish();
+                            startActivity(launchIntent);//null pointer check in case package name was not found
+                        }
+                        startActivity(launchIntent);
+                        countDownTimer.cancel();
+                        break;
+
+                    case 5 :
+                        Bundle mBundle5 = new Bundle();
+                        mBundle5.putString("ax", "metro");
+                        Intent intent5 = new Intent(getApplication() , AxActivity.class);
+                        intent5.putExtras(mBundle5);
+                        startActivity(intent5);
+                        countDownTimer.cancel();
+                        break;
+
+                    case 6 :
+//                        Bundle mBundle6 = new Bundle();
+//                        mBundle6.putString("ax", "brt");
+//                        Intent intent6 = new Intent(getApplication() , Main5Activity.class);
+//                        intent6.putExtras(mBundle6);
 //                        startActivity(intent6);
 //                        countDownTimer.cancel();
 //                        break;
-//
-//                    case  7 :
-//                        Bundle mBundle7 = new Bundle();
-//                        mBundle7.putString("url", "http://www.aut.ac.ir/aut/fa/");
-//                        Intent intent7 = new Intent(getApplication() , Main4Activity.class);
-//                        intent7.putExtras(mBundle7);
-//                        startActivity(intent7);
-//                        countDownTimer.cancel();
-//                        break;
-//
-//                    case  8 :
-//                        Bundle mBundle8 = new Bundle();
-//                        mBundle8.putString("url", "http://edu.aut.ac.ir/iCMS/ed/");
-//                        Intent intent8 = new Intent(getApplication() , Main4Activity.class);
-//                        intent8.putExtras(mBundle8);
-//                        startActivity(intent8);
-//                        countDownTimer.cancel();
-//                        break;
+
+                        Intent intent6 = new Intent(getApplication() , AxActivity.class);
+                        startActivity(intent6);
+                        countDownTimer.cancel();
+                        break;
+
+                    case  7 :
+                        Bundle mBundle7 = new Bundle();
+                        mBundle7.putString("url", "http://www.aut.ac.ir/aut/fa/");
+                        Intent intent7 = new Intent(getApplication() , WebPageActivity.class);
+                        intent7.putExtras(mBundle7);
+                        startActivity(intent7);
+                        countDownTimer.cancel();
+                        break;
+
+                    case  8 :
+                        Bundle mBundle8 = new Bundle();
+                        mBundle8.putString("url", "http://edu.aut.ac.ir/iCMS/ed/");
+                        Intent intent8 = new Intent(getApplication() , WebPageActivity.class);
+                        intent8.putExtras(mBundle8);
+                        startActivity(intent8);
+                        countDownTimer.cancel();
+                        break;
 
                 }
             }
